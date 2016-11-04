14fd852f0ce2fa36360a74f3299eb6437be8d1ee
kekee000 <kekee000@gmail.com>
2016-11-04 15:55:30 +0800

    update mip html

--- test/spec/validate.spec.js
+++ test/spec/validate.spec.js
@@ -6,7 +6,7 @@ const TMP_PATH = path.resolve(process.cwd(), 'test/test-project');
 const assert = require('assert');
 
 describe('validate.js ', function () {
-    before(function () {
+    beforeEach(function () {
         cli.__log = cli.log;
         // 代理log内容
         cli.log = function () {
@@ -15,14 +15,29 @@ describe('validate.js ', function () {
         };
     });
 
-    after(function() {
+    afterEach(function() {
         cli.log = cli.__log;
         delete cli.__logMessage;
         delete cli.__log;
     });
 
     // test case
-    it('validate', function (done) {
+    it('validate success', function (done) {
+
+        cmdValidate.exec({
+            files: ['mip.html'],
+            baseDir: TMP_PATH
+        });
+
+        setTimeout(() => {
+            assert.ok(cli.__logMessage.indexOf('validate success') >= 0, 'validate success');
+            done();
+        }, 10);
+
+    });
+
+    // test case
+    it('validate error', function (done) {
 
         cmdValidate.exec({
             files: ['validate.html'],
@@ -30,10 +45,10 @@ describe('validate.js ', function () {
         });
 
         setTimeout(() => {
-            assert.ok(cli.__logMessage, 'error exits');
-            assert.ok(cli.__logMessage.match(/ERROR/g)[4], '5 errors');
+            assert.ok(cli.__logMessage.indexOf('validate error') >= 0, 'validate error');
+            assert.ok(cli.__logMessage.match(/ERROR/g)[1], '>=2 errors');
             done();
-        }, 20);
+        }, 10);
 
     });
 });
